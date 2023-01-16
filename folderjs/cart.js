ee281c08077145e68a8776450c61a0f16bf4a1b4
gjohn <juansgb316@gmail.com>
2023-01-16 14:52:51 -0300

    eliminar prods de carrito listo

---
+++ folderjs/cart.js
@@ -0,0 +1,43 @@
+
+
+function addtocart(id){
+    const item = prodarray.find((prod) =>prod.id === id)
+    cart.push(item)
+    showcart();
+}
+function elimprod(id){
+    const prodID = id;
+    cart = cart.filter((prod)=>prod.id !== prodID)
+    showcart();
+    console.log(cart);
+}
+
+const showcart = ()=>{
+    const modalbody = document.querySelector(".modal .modal-body")
+    
+    modalbody.innerHTML = ""
+    cart.forEach((prod)=>{
+        let cant = 1;
+        const { id, marca, nombre, cat, precio, prodimage} = prod
+        modalbody.innerHTML +=`
+        <div class="cart-prod d-flex">
+            <figure class="cart-contimg img-fluid">
+                <img class="cart-contimg-img" src="${prodimage}">
+            </figure>
+            <div>
+            <p class="m-0">${cat}</p>
+                <p class="m-0">${marca}</p>
+                <p class="m-0">${nombre}</p>
+                <p class="m-0">Cantidad: ${cant}</p>
+                <p class="m-0">$${cant*precio}</p>
+            </div>
+            <div class="botonescart">
+            <button type="button" class="btn btn-success">+</button>
+            <button type="button" class="btn btn-danger">-</button>
+            <button type="button" onclick="elimprod(${id})" class="btn btn-warning">Eliminar</button>
+            </div>
+
+        </div>
+        `
+    })
+}
---
+++ folderjs/indexapp.js
@@ -0,0 +1,19 @@
+//Product card admin
+let prodcard = document.getElementById(`prodcard`);
+prodarray.forEach((prod) =>{
+    const { id, marca, nombre, cat, precio, prodimage, cant} = prod
+let contentsectioncard = document.createElement("ARTICLE")
+prodcard.appendChild(contentsectioncard);
+contentsectioncard.innerHTML = `
+<div class="card__imgcontainer_prod">
+<img class="card__img__prod" src="${prodimage}">
+</div>
+<div class="card__text">
+<div class="card__text__prod">${nombre}</div>
+<div class="card__price__prod">$${precio}</div>
+<div class="card__cart__prod">
+<button onclick="addtocart(${id})">A</button></div>
+</div>
+`
+})
+
